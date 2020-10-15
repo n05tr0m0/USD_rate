15da9bf7ee19a0cb2f083658b4dd2b2b410b87c1
Muslim Abdulmazhidov <htrx101@gmail.com>
2020-10-15 19:50:23 +0300

    connection error exception added

--- get_usd_rate.py
+++ get_usd_rate.py
@@ -7,32 +7,42 @@ by n05tr0m0
 import pync
 import requests
 from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
 
 
-def get_html():
+def get_html_page() -> str:
     url = 'http://cbr.ru/'
     headers = {
         'user-agent': 'Mozilla/5.0 () AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 '
                       'OPR/67.0.3575.79'
     }
-    html_ = requests.get(url, headers=headers)
-    return html_.text
+
+    try:
+        resp = requests.get(url, headers=headers)
+    except ConnectionError:
+        resp = 'Нет соединения'
+
+    return resp if isinstance(resp, str) else resp.text
 
 
-def get_dollar_rate(html):
-    soup = BeautifulSoup(html, 'lxml')
+def get_dollar_rate(source_text: str) -> str:
+    if source_text == 'Нет соединения':
+        return 'Нет соединения'
+    soup = BeautifulSoup(source_text, 'lxml')
     result = soup.find('div', class_='indicator_el_value mono-num').text
     clear_result = result[:-3]
     return clear_result
 
 
 def send_notification(message):
-    title = 'Курс Доллара США'
+    if message == 'Нет соединения':
+        return pync.notify('Невозможно получить данные', title='Ошибка сети')
+    title = 'Доллара США по курсу ЦБ РФ'
     return pync.notify(f'На сегодня {message} руб.', title=title)
 
 
 def main():
-    send_notification(get_dollar_rate(get_html()))
+    send_notification(get_dollar_rate(get_html_page()))
 
 
 if __name__ == '__main__':
