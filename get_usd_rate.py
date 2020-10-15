37155a4d16aa31d8451680495bfbfcddf4440d70
Muslim Abdulmazhidov <htrx101@gmail.com>
2020-10-15 20:32:03 +0300

    Save last course in local storage and update README.MD

--- get_usd_rate.py
+++ get_usd_rate.py
@@ -3,6 +3,7 @@
 
 by n05tr0m0
 """
+from datetime import date
 
 import pync
 import requests
@@ -10,7 +11,7 @@ from bs4 import BeautifulSoup
 from requests.exceptions import ConnectionError
 
 
-def get_html_page() -> str:
+def get_html_page() -> str or None:
     url = 'http://cbr.ru/'
     headers = {
         'user-agent': 'Mozilla/5.0 () AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36 '
@@ -25,18 +26,33 @@ def get_html_page() -> str:
     return resp if isinstance(resp, str) else resp.text
 
 
+def save_last_course(usd_value: str):
+    with open('data', 'w', encoding='utf-8') as f:
+        f.write(f'На {date.today().strftime("%d.%m.%Y")} курс доллара: {usd_value} руб')
+
+
 def get_dollar_rate(source_text: str) -> str:
     if source_text == 'Нет соединения':
         return 'Нет соединения'
     soup = BeautifulSoup(source_text, 'lxml')
     result = soup.find('div', class_='indicator_el_value mono-num').text
     clear_result = result[:-3]
+    save_last_course(clear_result)
     return clear_result
+
+
+def load_last_course() -> str:
+    with open('data', 'r', encoding='utf-8') as f:
+        return f.read()
 
 
 def send_notification(message):
     if message == 'Нет соединения':
-        return pync.notify('Невозможно получить данные', title='Ошибка сети')
+        try:
+            last_course = load_last_course()
+        except FileNotFoundError:
+            return pync.notify('Невозможно получить данные', title='Ошибка')
+        return pync.notify(last_course, title='Нет сети. Последние сохранённые данные.')
     title = 'Доллара США по курсу ЦБ РФ'
     return pync.notify(f'На сегодня {message} руб.', title=title)
 
